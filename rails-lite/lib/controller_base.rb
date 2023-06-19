593af0401080cdcaf1e971f83eb566863db46c66
Viktoria Czaran <vczaran@gmail.com>
2023-06-19 17:13:18 -0400

    Rails lite attempt

--- rails-lite/lib/controller_base.rb
+++ rails-lite/lib/controller_base.rb
@@ -8,20 +8,32 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    !!@already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    @res.status = 302
+    @res["Location"] = url
+
+    raise "rendering twice" if already_built_response?
+    @already_built_response = @res
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    @res['Content-Type'] = content_type
+
+    raise "rendering twice" if already_built_response?
+    @already_built_response = @res
   end
 
   # use ERB and binding to evaluate templates
